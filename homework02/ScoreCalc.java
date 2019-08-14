38b698604934d7469935c4e763562b53e1edb02e
leja0418 <leja0418@gmail.com>
2019-08-14 19:27:59 +0900

    190814 18:55

--- homework02/ScoreCalc.java
+++ homework02/ScoreCalc.java
@@ -1,5 +1,7 @@
 package homework02;
 
+import java.util.Scanner;
+
 public class ScoreCalc {
 
 	public static void main(String[] args) {
@@ -26,6 +28,37 @@ public class ScoreCalc {
 		현재까지의 누적값은 50 + 36 + 78 + 77 = 348 이고, 평균은 87 입니다. (소수점이하는 절삭)
 		 */
 
+		Scanner sc = new Scanner(System.in);
+		int  Acc= 0;
+		int loop = 0;
+		int avg= 0;
+		String AccString= "";
+		
+		
+		while(true) {
+			System.out.println("더하시려는 숫자를 입력하세요(종료는 -1)");
+			int num =0;
+			num = sc.nextInt();
+			if (num == -1) {
+				AccString = 	AccString.substring(0, AccString.length()-1);
+				System.out.printf("현재까지의 누적값은 %s = %d 이고, 평균은 %d 입니다.", AccString , Acc , Acc/loop);
+				break;
+			} 
+			
+			Acc += num;
+			AccString += num + "+";
+			loop +=1;
+		}
+		
+		
+		
+		
 	}
+	
+	
+	
+	
+	
+	
 
 }
